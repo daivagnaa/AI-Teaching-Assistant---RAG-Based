98914c7b9463e13d7c2426a634d625f7d8e50851
daivagnaa <devparmar1895@gmail.com>
2026-06-29 19:56:57 +0530

    Remove unused vector scripts from repository

--- 2. Text To Vector/metadata.py
+++
@@ -1,9 +0,0 @@
-import faiss
-import numpy as np
-import pandas as pd
-
-df = pd.read_pickle("metadata.pkl")
-
-print(df.shape)
-print(df.columns)
-print(df)
--- 2. Text To Vector/test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_pickle("embeddings.pkl")
-
-print(df)
